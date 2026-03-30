5b1fe07ae3004a19754a3d4befc9f42ab661ec0c
LothaireE <lothaire.epee@gmail.com>
2026-03-30 12:06:09 +0200

    lint

--- src/library/graphql/queries/products.ts
+++ src/library/graphql/queries/products.ts
@@ -11,6 +11,7 @@ export const GET_PRODUCTS = gql`
                         username
                     }
                 }
+                unitPrice
                 images {
                     publicId
                     url
